8471e8cad2589ba1574ed85a8603b8b74e37f875
Simon Hempenstall <simonjhemp@hotmail.com>
2017-10-29 20:22:03 +0000

    Refactor controller by adding api data method

--- lib/controller.rb
+++ lib/controller.rb
@@ -3,11 +3,16 @@ require './lib/user.rb'
 
 class Controller
 
-  attr_reader :api, :user
+  attr_reader :api, :user, :users_data, :purchases_data
 
   def initialize(api = Api.new, user = User.new)
     @api = api
     @user = user
+  end
+
+  def api_data
+    @users_data = api.get_user_data
+    @purchases_data = api.get_purchase_data
   end
 
   def run(args)
@@ -21,15 +26,11 @@ class Controller
   end
 
   def user_total_spend(email)
-    users_data = api.get_user_data
-    purchases_data = api.get_purchase_data
     user_id = user.find_id(email, users_data)
     puts user.total_spend(user_id, purchases_data)
   end
 
   def user_average_spend(email)
-    users_data = api.get_user_data
-    purchases_data = api.get_purchase_data
     user_id = user.find_id(email, users_data)
     puts user.average_spend(user_id, purchases_data)
   end
