14d645e4c6bc183c21c9a72a2b7884c7dc567d54
Simon Hempenstall <simonjhemp@hotmail.com>
2017-10-29 20:43:58 +0000

    Revert changes as command line prompts failing

--- lib/controller.rb
+++ lib/controller.rb
@@ -3,16 +3,11 @@ require './lib/user.rb'
 
 class Controller
 
-  attr_reader :api, :user, :users_data, :purchases_data
+  attr_reader :api, :user
 
   def initialize(api = Api.new, user = User.new)
     @api = api
     @user = user
-  end
-
-  def api_data
-    @users_data = api.get_user_data
-    @purchases_data = api.get_purchase_data
   end
 
   def run(args)
@@ -26,11 +21,15 @@ class Controller
   end
 
   def user_total_spend(email)
+    users_data = api.get_user_data
+    purchases_data = api.get_purchase_data
     user_id = user.find_id(email, users_data)
     puts user.total_spend(user_id, purchases_data)
   end
 
   def user_average_spend(email)
+    users_data = api.get_user_data
+    purchases_data = api.get_purchase_data
     user_id = user.find_id(email, users_data)
     puts user.average_spend(user_id, purchases_data)
   end
